80269e3324b08ca245391adc0d86c710b33d4e74
espartancasta <enriquecasta117@gmail.com>
2025-09-28 23:32:50 -0600

    add admin layout with flowbite sidebar, navigation includes, and dashboard slot

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -6,7 +6,7 @@ use App\Models\User;
 use Illuminate\Database\Seeder;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
-
+//uso this kind
 class DatabaseSeeder extends Seeder
 {
     /**
